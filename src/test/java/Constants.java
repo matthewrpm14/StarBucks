e94163b9928dc1c6b3c0da4b25d999e379f0aa69
Matthew Robbins <matthewr@perfectomobile.com>
2019-11-18 17:01:34 -0500

    Update Constants.java

--- src/test/java/Constants.java
+++ src/test/java/Constants.java
@@ -6,10 +6,7 @@ public class Constants
 
 
 //	public static final String PM_USER = "uzie@perfectomobile.com";
-	public static final String PM_PASSWORD = "tda123";
-	public static final String PM_USER = "automation2@tdameritrade.com";
 
-	public static final String PM_CLOUD = "tda.perfectomobile.com";
 	
 	public static final String REPORT_LIB = "c:\\TDA\\";
 
